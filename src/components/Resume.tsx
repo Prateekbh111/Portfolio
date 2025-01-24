b44412f15d556e6aacc034e3f007c3f6301188eb
Prateek <prateekbh111@gmail.com>
2025-01-24 21:29:32 +0530

    update dependencies and refactor code formatting

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -3,35 +3,35 @@ import { ContainerScroll } from "./ui/container-scroll-animation";
 import Image from "next/image";
 
 export default function Resume() {
-	return (
-		<div className="flex flex-col overflow-hidden bg-white dark:bg-black">
-			<ContainerScroll
-				titleComponent={
-					<>
-						<h1 className="text-xl md:text-4xl font-semibold text-black dark:text-white">
-							Take a look at my <br />
-							<span className="text-4xl md:text-8xl font-bold mt-1 leading-none">
-								Curriculum Vitae
-							</span>
-						</h1>
-					</>
-				}
-			>
-				<Link
-					href={
-						"https://drive.google.com/file/d/1GO_e_zbCtiCnnkiLLuV-FRgnYNsoC6Gv/view?usp=sharing"
-					}
-					target="_blank"
-				>
-					<Image
-						src="/Prateek_CV.png"
-						alt=""
-						height={720}
-						width={1400}
-						className="mx-auto rounded-2xl object-cover h-full object-left-top"
-					/>
-				</Link>
-			</ContainerScroll>
-		</div>
-	);
+  return (
+    <div className="flex flex-col overflow-hidden bg-white dark:bg-black">
+      <ContainerScroll
+        titleComponent={
+          <>
+            <h1 className="text-xl md:text-4xl font-semibold text-black dark:text-white">
+              Take a look at my <br />
+              <span className="text-4xl md:text-8xl font-bold mt-1 leading-none">
+                Curriculum Vitae
+              </span>
+            </h1>
+          </>
+        }
+      >
+        <Link
+          href={
+            "https://drive.google.com/file/d/1YVW_X3__PBqaPetD6YjW8qj4Gn24SBF0/view?usp=drive_link"
+          }
+          target="_blank"
+        >
+          <Image
+            src="/Prateek_CV.jpg"
+            alt=""
+            height={720}
+            width={1400}
+            className="mx-auto rounded-2xl object-cover h-full object-left-top"
+          />
+        </Link>
+      </ContainerScroll>
+    </div>
+  );
 }
